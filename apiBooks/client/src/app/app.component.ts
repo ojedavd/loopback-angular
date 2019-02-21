f6881f027e9eaa1035186f7633fee1fde71892e6
ssvictoro@salud.gob.ar <ssvictoro@salud.gob.ar>
2019-02-21 08:39:22 -0300

    Comprobar si el usuario está logado - Angular 6

--- apiBooks/client/src/app/app.component.ts
+++ apiBooks/client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front';
+  constructor(private router: Router) {
+      this.router.events.subscribe(event => {
+          if (event instanceof NavigationEnd) {
+              if (event.url.match('/user/') || event.url.match('/admin/')) {
+                  this.hideElement = true;
+              } else {
+                  this.hideElement = false;
+              }
+          }
+      });
+  }
+
+  public hideElement = false;
+
 }
--- apiBooks/client/src/app/components/navbar/navbar.component.ts
+++ apiBooks/client/src/app/components/navbar/navbar.component.ts
@@ -8,16 +8,24 @@ import { Location } from '@angular/common';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
   constructor(private authService: AuthService, private location: Location) { }
   public app_name = 'Books Store';
   public isLogged = false;
 
   ngOnInit() {
+    this.onCheckUser();
   }
 
   onLogout(): void {
     this.authService.logoutUser();
     location.reload();
+  }
+
+  onCheckUser(): void {
+    if (this.authService.getCurrentUser() === null) {
+      this.isLogged = false;
+    } else {
+      this.isLogged = true;
+    }
   }
 }
